1a745a19173841082cedadcb4b5f6cf58f4748bc
EzePicc <epiccardobelen@byupathway.org>
2026-05-09 17:11:55 -0300

    Finished prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Data.Common;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade (%)? ");
+        string input = Console.ReadLine();
+        int grade_percent = int.Parse(input);
+
+        if (grade_percent >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+            Console.WriteLine("Great job! You passed the course");
+        }
+        else if (grade_percent >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+            Console.WriteLine("Great job! You passed the course");
+        }
+        else if (grade_percent >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+            Console.WriteLine("Good job! You passed the course");
+        }
+        else if (grade_percent >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+            Console.WriteLine("Good job! You passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+            Console.WriteLine("You didn't pass the course. You can do it next time!");
+        }
     }
 }
