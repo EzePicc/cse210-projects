35d3decfe337b29c0573a783d3b359869c346a7d
EzePicc <epiccardobelen@byupathway.org>
2026-05-23 15:32:13 -0300

    fixed errrors. Now finished

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -4,7 +4,13 @@ class Reference
     private int _chapter;
     private int _verse;
     private int _endVerse;
-
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
+    }
     public Reference(string book, int chapter, int verse, int endVerse)
     {
         _book = book;
@@ -24,3 +30,4 @@ class Reference
             return $"{_book} {_chapter}:{_verse}-{_endVerse}";
         }
     }
+}
