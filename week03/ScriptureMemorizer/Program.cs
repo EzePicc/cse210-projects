97369e6f5f736b9f2b2c793a7eac5150bed63e6f
EzePicc <epiccardobelen@byupathway.org>
2026-05-23 15:57:34 -0300

    Finished without creativity

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+    static void Main()
+    {        
+        Scripture scripture = new Scripture(
+            new Reference("John", 3, 16),
+            "For God so loved the world that he gave his one and only Son that whoever believes in him shall not perish but have eternal life"
+        );
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\n");
+
+            if (scripture.IsCompletelyHidden())
+            {
+                
+                break;
+            }
+
+            Console.Write("Press Enter or write 'quit': ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
