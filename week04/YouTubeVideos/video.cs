ef0e695457a0311133bbda2b7c9464d8de648473
EzePicc <epiccardobelen@byupathway.org>
2026-05-30 14:28:24 -0300

    Add ";"

--- week04/YouTubeVideos/video.cs
+++ week04/YouTubeVideos/video.cs
@@ -3,7 +3,7 @@ public class Video
     private string _title;
     private string _author;
     private int _lengthInSeconds;
-    private List<Comment> _comments = new List<Comment>()
+    private List<Comment> _comments = new List<Comment>();
 
     public Video(string title, string author, int lengthInSeconds)
     {
