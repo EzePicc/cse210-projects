efa3f8055c9d6bb90f2e534493cadaacb28f617e
EzePicc <epiccardobelen@byupathway.org>
2026-06-13 18:07:07 -0300

    Add check list

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,25 @@
+public class ChecklistGoal : Goal
+{    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus): base(name, description, points)
+    {
+        _amountCompleted  =0;
+        _target= target;
+        _bonus=bonus;
+    }
+    public override void RecordEvent(){ }
+    public override bool IsComplete()
+    {
+        return _amountCompleted>= _target;
+
+    }
+
+    public override string GetDetailsString()
+    {
+        string box = IsComplete()? "[X]" : "[ ]";
+        return $"{box} {_shortName} ({_description})-- Completed {_amountCompleted} / {_target}";
+    }
+
+}
